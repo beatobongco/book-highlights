9a19b90677a1604cb2e0d463878d10f67a1b1131
Beato Bongco <beatobongco@gmail.com>
2018-04-13 02:21:58 +0800

    Remove unused codecs package

--- generate_pages.py
+++ generate_pages.py
@@ -1,4 +1,4 @@
-import os, codecs
+import os
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(
@@ -7,13 +7,12 @@ env = Environment(
 )
 
 template = env.get_template('book.html')
-UTF8Writer = codecs.getwriter('utf8')
-# https://stackoverflow.com/questions/22181944/using-utf-8-characters-in-a-jinja2-template/22182709
 
 for filename in os.listdir('raw_notes'):
   with open(os.getcwd() + '/raw_notes/' + filename, 'r') as file_in:
     content = file_in.readlines()
     raw_notes = ''.join(content[3:])
+    # https://stackoverflow.com/questions/22181944/using-utf-8-characters-in-a-jinja2-template/22182709
     rendered = template.render(title=content[0].strip(), summary=content[1].strip(),
                                short_summary=content[2].strip(), raw_notes=raw_notes.decode('utf-8'))
     with open(os.getcwd() + '/book/' + filename + '.html', 'w+') as file_out:
